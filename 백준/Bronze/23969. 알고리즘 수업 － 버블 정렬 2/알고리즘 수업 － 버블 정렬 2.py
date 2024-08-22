90847a849841666bb0acffefdd953d1fb36d8c9f
Rag117 <gjwnsgh1107@naver.com>
2024-08-22 20:39:03 +0900

    [Bronze I] Title: 알고리즘 수업 - 버블 정렬 2, Time: 336 ms, Memory: 110696 KB -BaekjoonHub

---
+++ 백준/Bronze/23969. 알고리즘 수업 － 버블 정렬 2/알고리즘 수업 － 버블 정렬 2.py
@@ -0,0 +1,13 @@
+a, b = map(int, input().split())
+arr = list(map(int, input().split()))
+cnt = 0
+
+for i in range(a-1, 0, -1): 
+    for j in range(i): 
+        if arr[j] > arr[j+1]:
+            arr[j], arr[j+1] = arr[j+1], arr[j]
+            cnt += 1
+            if cnt == b:
+                print(*arr)
+                exit()
+print(-1)
